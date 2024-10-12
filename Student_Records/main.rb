4813a4ae5eef9cfb412f663d5d6453b237fb188c
Maks0nchick <maksimkrapotkin@gmail.com>
2024-10-12 13:24:14 +0300

    |Lab №2|Zadanie №1,№3,№4|change_appealing_an_object|
    Создать класс Student в отдельном файле с полями
    объекта ID, Фамилия, Имя, Отчество, Телефон, Телеграм,
    Почта, Гит. ФИО – обязательно, остальные – нет.
    Написать конструктор, написать геттер и сеттер для
    каждого поля, при именовании методов ОБЯЗАТЕЛЬНО
    пользоваться соглашениями о наименованиях в ruby.
    Избежать дублирования кода в конструкторе, геттере и
    сеттере.
    Воспользоваться аттрибутами для краткого написания
    геттеров и сеттеров.

--- Student_Records/main.rb
+++ Student_Records/main.rb
@@ -1,7 +1,7 @@
 # main.rb
 require_relative 'student'
 
-student1 = Student.new(1, "Иванов", "Иван", "Иванович", "+79123456789", "@ivanov", "ivanov@mail.com", "github.com/ivanov")
+student1 = Student.new(1, "Иванов", "Иван", "Иванович", "+7-918-999-69-96", "@ivanov", "ivanov@mail.com", "github.com/ivanov")
 student2 = Student.new(2, "Петров", "Петр", "Петрович")
 
 puts student1
--- Student_Records/student.rb
+++ Student_Records/student.rb
@@ -1,22 +1,24 @@
 # student.rb
+#attr_reader создает только геттер для переменной экземпляра.
+#attr_writer создает только сеттер для переменной экземпляра.
+#attr_accessor создает и геттер, и сеттер для переменной экземпляра.
 class Student
   # Определяем геттеры и сеттеры для каждого поля
   attr_accessor :id, :last_name, :first_name, :middle_name, :phone, :telegram, :email, :github
 
-  # Конструктор
   def initialize(id, last_name, first_name, middle_name = nil, phone = nil, telegram = nil, email = nil, github = nil)
-    @id = id
-    @last_name = last_name
-    @first_name = first_name
-    @middle_name = middle_name
-    @phone = phone
-    @telegram = telegram
-    @email = email
-    @github = github
+    self.id = id
+    self.last_name = last_name
+    self.first_name = first_name
+    self.middle_name = middle_name
+    self.phone = phone
+    self.telegram = telegram
+    self.email = email
+    self.github = github
   end
 
   # Метод для красивого вывода информации об объекте
   def to_s
-    "ID: #{@id}, ФИО: #{@last_name} #{@first_name} #{@middle_name}, Телефон: #{@phone}, Телеграм: #{@telegram}, Почта: #{@email}, Гит: #{@github}"
+    "ID: #{self.id}, ФИО: #{self.last_name} #{self.first_name} #{self.middle_name}, Телефон: #{self.phone}, Телеграм: #{self.telegram}, Почта: #{self.email}, Гит: #{self.github}"
   end
 end
