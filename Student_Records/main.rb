648c6497feec24faa4986ad730af4e6bd0245acc
Maks0nchick <maksimkrapotkin@gmail.com>
2024-10-12 13:00:33 +0300

    |Lab №2|Zadanie №2|create_main|
    Создать несколько объектов из отдельного файла main.rb
    вывести информацию о них на экран. Продумать
    корректный способ вывода информации о текущем
    состоянии объекта на экран, модифицировать класс.

---
+++ Student_Records/main.rb
@@ -0,0 +1,8 @@
+# main.rb
+require_relative 'student'
+
+student1 = Student.new(1, "Иванов", "Иван", "Иванович", "+79123456789", "@ivanov", "ivanov@mail.com", "github.com/ivanov")
+student2 = Student.new(2, "Петров", "Петр", "Петрович")
+
+puts student1
+puts student2
