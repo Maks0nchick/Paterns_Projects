d4e7524b2bcd4756369cffd6e55dc3dee79ed43d
Maks0nchick <maksimkrapotkin@gmail.com>
2024-10-26 01:45:29 +0300

    |Lab№2|Zadanie №8, №9|method validate|
    Создать валидации для корректной формы строки в
    остальных полях.
    Написать метод
    validate, который проводит две
    валидации наличие гита и наличие любого контакта для
    связи, по возможности разделить методы.

--- Student_Records/Person.rb
+++ Student_Records/Person.rb
@@ -5,10 +5,52 @@ class Person
     phone_number.start_with?("+")
   end
 
+  def self.validate_last_name(last_name)
+    unless last_name.present?
+      raise ArgumentError, "Фамилия должна быть заполнена"
+    end
+  end
+
+  def self.validate_first_name(first_name)
+    unless first_name.present?
+      raise ArgumentError, "Имя должно быть заполнено"
+    end
+  end
+
+  def self.validate_middle_name(middle_name)
+    unless middle_name.present?
+      raise ArgumentError, "Отчество должно быть заполнено"
+    end
+  end
+
+  def self.validate_email(email)
+    unless email.present?
+      raise ArgumentError, "Email должен быть заполнен"
+    end
+  end
+
+  def self.validate_github(github)
+    unless github.present?
+      raise ArgumentError, "Github должен быть заполнен"
+    end
+  end
+
+  def self.validate_contacts(phone, telegram, email)
+    unless [phone, telegram, email].any?(&:present?)
+      raise ArgumentError, "Необходим хотя бы один контакт для связи"
+    end
+  end
+
   def initialize(attributes)
+    self.class.validate_last_name(attributes[:last_name])
+    self.class.validate_first_name(attributes[:first_name])
+    self.class.validate_middle_name(attributes[:middle_name])
+    self.class.validate_email(attributes[:email])
+    self.class.validate_github(attributes[:github])
     unless attributes[:phone].nil? || self.class.check_phone_number(attributes[:phone])
       raise ArgumentError, "Телефонный номер должен начинаться с '+'"
     end
+    self.class.validate_contacts(attributes[:phone], attributes[:telegram], attributes[:email])
     @attributes = attributes
     @id = @attributes[:id]
     @last_name = @attributes[:last_name]
@@ -22,5 +64,14 @@ class Person
 
   def to_s
     "Человек #{@id}: #{@last_name} #{@first_name} #{@middle_name}, телефон: #{@phone || 'не указан'}, телеграм: #{@telegram || 'не указан'}, email: #{@email || 'не указан'}, github: #{@github || 'не указан'}"
+  end
+
+  def validate
+    self.class.validate_last_name(@last_name)
+    self.class.validate_first_name(@first_name)
+    self.class.validate_middle_name(@middle_name)
+    self.class.validate_email(@email)
+    self.class.validate_github(@github)
+    self.class.validate_contacts(@phone, @telegram, @email)
   end
 end
--- Student_Records/main.rb
+++ Student_Records/main.rb
@@ -9,14 +9,32 @@ puts student2.to_s
 
 require_relative 'person'
 
-person = Person.new(id: 1, last_name: "Иванов", first_name: "Иван", middle_name: "Иванович", phone: "+7-918-999-69-96", telegram: "@ivanov", email: "ivanov@mail.com", github: "github.com/ivanov")
-puts person.to_s
+person = Person.new(
+  id: 1,
+  last_name: "Иванов",
+  first_name: "Иван",
+  middle_name: "Иванович",
+  phone: "+7-918-999-69-96",
+  telegram: "@ivanov",
+  email: "ivanov@mail.com",
+  github: "github.com/ivanov"
+)
 
 person1 = Person.new(id: 1, last_name: "Иванов", first_name: "Иван", middle_name: "Иванович", phone: "+7-918-999-69-96", telegram: "@ivanov", email: "ivanov@mail.com", github: "github.com/ivanov")
 puts person1.to_s
 
+
 begin
-  person2 = Person.new(id: 2, last_name: "Петров", first_name: "Петр", middle_name: "Петрович", phone: "1234567890", telegram: "@petrov", email: "petrov@mail.com", github: "github.com/petrov")
+  person2 = Person.new(
+    id: 2,
+    last_name: "Петров",
+    first_name: "Петр",
+    middle_name: "Петрович",
+    phone: "1234567890",
+    telegram: "@petrov",
+    email: "petrov@mail.com",
+    github: "github.com/petrov"
+  )
 rescue ArgumentError => e
   puts e.message
 end
