466739093fbadf69d94ec441790c308df218ac37
Maks0nchick <maksimkrapotkin@gmail.com>
2024-11-01 22:50:24 +0300

    |Lab№2|Zadanie №16|refactoring|
    Провести рефакторинг, выделив суперкласс и убрав
    дублирование кода в классах Student и Student_short.

--- Student_Records/Person.rb
+++ Student_Records/Person.rb
@@ -1,83 +1,29 @@
 class Person
-  attr_reader :id, :last_name, :first_name, :middle_name, :phone, :telegram, :email, :github
+  attr_reader :id, :last_name_initials, :github, :contact
 
-  def self.check_phone_number(phone_number)
-    phone_number.start_with?("+")
+  def initialize(id:, last_name_initials:, github:, contact:)
+    @id = id
+    @last_name_initials = last_name_initials
+    @github = github
+    @contact = contact
   end
 
-  def self.validate_last_name(last_name)
-    unless last_name.present?
-      raise ArgumentError, "Фамилия должна быть заполнена"
-    end
+  def get_info
+    "#{last_name_initials}; GitHub: #{github}; Контакт: #{contact[:type]} (#{contact[:value]})"
   end
+end
 
-  def self.validate_first_name(first_name)
-    unless first_name.present?
-      raise ArgumentError, "Имя должно быть заполнено"
-    end
+class Student < Person
+  def initialize(id:, last_name:, first_name:, middle_name:, github:, contact:)
+    last_name_initials = "#{last_name} #{first_name[0]}.#{middle_name[0]}."
+    super(id: id, last_name_initials: last_name_initials, github: github, contact: contact)
   end
+end
 
-  def self.validate_middle_name(middle_name)
-    unless middle_name.present?
-      raise ArgumentError, "Отчество должно быть заполнено"
-    end
-  end
-
-  def self.validate_email(email)
-    unless email.present?
-      raise ArgumentError, "Email должен быть заполнен"
-    end
-  end
-
-  def self.validate_github(github)
-    unless github.present?
-      raise ArgumentError, "Github должен быть заполнен"
-    end
-  end
-
-  def self.validate_contacts(phone, telegram, email)
-    unless [phone, telegram, email].any?(&:present?)
-      raise ArgumentError, "Необходим хотя бы один контакт для связи"
-    end
-  end
-
-  def initialize(attributes)
-    self.class.validate_last_name(attributes[:last_name])
-    self.class.validate_first_name(attributes[:first_name])
-    self.class.validate_middle_name(attributes[:middle_name])
-    self.class.validate_email(attributes[:email])
-    self.class.validate_github(attributes[:github])
-    unless attributes[:phone].nil? || self.class.check_phone_number(attributes[:phone])
-      raise ArgumentError, "Телефонный номер должен начинаться с '+'"
-    end
-    self.class.validate_contacts(attributes[:phone], attributes[:telegram], attributes[:email])
-    @attributes = attributes
-    @id = @attributes[:id]
-    @last_name = @attributes[:last_name]
-    @first_name = @attributes[:first_name]
-    @middle_name = @attributes[:middle_name]
-    @phone = @attributes[:phone]
-    @telegram = @attributes[:telegram]
-    @email = @attributes[:email]
-    @github = @attributes[:github]
-  end
-
-  def to_s
-    "Человек #{@id}: #{@last_name} #{@first_name} #{@middle_name}, телефон: #{@phone || 'не указан'}, телеграм: #{@telegram || 'не указан'}, email: #{@email || 'не указан'}, github: #{@github || 'не указан'}"
-  end
-
-  def set_contacts(phone: nil, telegram: nil, email: nil)
-    @phone = phone if phone.present?
-    @telegram = telegram if telegram.present?
-    @email = email if email.present?
-  end
-
-  def validate
-    self.class.validate_last_name(@last_name)
-    self.class.validate_first_name(@first_name)
-    self.class.validate_middle_name(@middle_name)
-    self.class.validate_email(@email)
-    self.class.validate_github(@github)
-    self.class.validate_contacts(@phone, @telegram, @email)
+class StudentShort < Person
+  def self.from_data(id, data_string)
+    last_name_initials, github, contact_type, contact_value = data_string.split("; ")
+    contact = { type: contact_type.split(":").first, value: contact_type.split(":").last }
+    new(id: id, last_name_initials: last_name_initials, github: github, contact: contact)
   end
 end
