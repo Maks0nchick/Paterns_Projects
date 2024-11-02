6eca875bdb07cf27a5f933d8c135920e127ce767
Maks0nchick <maksimkrapotkin@gmail.com>
2024-11-02 13:26:51 +0300

    |Lab №2|Zadanie №1.8|creat validate for string form|
    Создать валидации для корректной формы строки в
    остальных полях.

--- Student_Records/class_student.rb
+++ Student_Records/class_student.rb
@@ -1,29 +1,51 @@
 class Student < Person
   attr_accessor :phone, :telegram, :email
 
-  def initialize(required_options = {}, optional_options = {}) #проверка наличия обязательных полей и применять значения устанавливая nil
-    # Инициализация полей Person
+  def initialize(required_options = {}, optional_options = {})
     super(required_options[:firstname], required_options[:lastname], required_options[:surname], optional_options[:id], optional_options[:git])
 
-    # Установка номера телефона с проверкой
     set_phone(optional_options.fetch(:phone, nil))
-    
-    # Установка других контактных данных
-    @telegram = optional_options.fetch(:telegram, nil)
-    @email = optional_options.fetch(:email, nil)
+    set_telegram(optional_options.fetch(:telegram, nil))
+    set_email(optional_options.fetch(:email, nil))
   end
 
-  # Метод класса для проверки корректности телефонного номера
+  # Валидация для телефонного номера
   def self.valid_phone_number?(phone)
     phone_regex = /^\+?\d{10,15}$/
     phone =~ phone_regex
   end
 
-  # Метод для установки номера телефона с проверкой
+  # Установка номера телефона с проверкой
   def set_phone(phone)
     raise ArgumentError, "Некорректный номер телефона: #{phone}" unless self.class.valid_phone_number?(phone)
 
     @phone = phone
+  end
+
+  # Валидация для Telegram
+  def self.valid_telegram?(telegram)
+    telegram_regex = /^@[\w\d_]{5,}$/
+    telegram =~ telegram_regex
+  end
+
+  # Установка Telegram с проверкой
+  def set_telegram(telegram)
+    raise ArgumentError, "Некорректный Telegram: #{telegram}" unless self.class.valid_telegram?(telegram)
+
+    @telegram = telegram
+  end
+
+  # Валидация для email
+  def self.valid_email?(email)
+    email_regex = /^[\w.+-]+@[\w-]+\.[\w.-]+$/
+    email =~ email_regex
+  end
+
+  # Установка email с проверкой
+  def set_email(email)
+    raise ArgumentError, "Некорректный email: #{email}" unless self.class.valid_email?(email)
+
+    @email = email
   end
 
   # Метод для форматированного вывода информации об объекте
