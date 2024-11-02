d35d58446d35e082715391b9c7e963902903fe82
Maks0nchick <maksimkrapotkin@gmail.com>
2024-11-02 13:01:50 +0300

    |Lab №2|Zadanie №1.5|atribut check and change main|
    Обеспечить в конструкторе возможность создания
    объектов с любой комбинацией заполнения необязательных полей.

--- Student_Records/class_student.rb
+++ Student_Records/class_student.rb
@@ -3,18 +3,33 @@ class Student
 
   # Конструктор класса, принимающий параметры через хеш options
   def initialize(**options)
-    options.each do |key, value|
-      instance_variable_set("@#{key}", value)
+    # Проверяем обязательные поля
+    required_fields = [:firstname, :lastname, :surname]
+    missing_fields = required_fields.select { |field| options[field].nil? }
+    
+    # Если обязательные поля отсутствуют, выбрасываем ошибку
+    unless missing_fields.empty?
+      raise ArgumentError, "Отсутствуют обязательные поля: #{missing_fields.join(', ')}"
     end
+
+    # Устанавливаем значения для всех полей, задавая nil для отсутствующих
+    @firstname = options[:firstname]
+    @lastname = options[:lastname]
+    @surname = options[:surname]
+    @id = options[:id]
+    @phone = options[:phone]
+    @telegram = options[:telegram]
+    @email = options[:email]
+    @git = options[:git]
   end
 
   # Метод для форматированного вывода информации об объекте
   def to_s
     info = "Информация о студенте:\n"
     info += "ID: #{@id}\n" if @id
-    info += "Фамилия: #{@lastname}\n" if @lastname
-    info += "Имя: #{@firstname}\n" if @firstname
-    info += "Отчество: #{@surname}\n" if @surname
+    info += "Фамилия: #{@lastname}\n"
+    info += "Имя: #{@firstname}\n"
+    info += "Отчество: #{@surname}\n"
     info += "Телефон: #{@phone}\n" if @phone
     info += "Telegram: #{@telegram}\n" if @telegram
     info += "Почта: #{@email}\n" if @email
--- Student_Records/main.rb
+++ Student_Records/main.rb
@@ -1,13 +1,20 @@
 require_relative 'student'
 
-# Создание объектов студентов
-student1 = Student.new(firstname: "Иван", lastname: "Иванов", surname: "Иванович", id: "001", phone: "+79991234567", telegram: "ivan_ivanov", email: "ivan@example.com", git: "https://github.com/ivan")
-student2 = Student.new(firstname: "Петр", lastname: "Петров", surname: "Петрович", id: "002", telegram: "@petrov", email: "petr@example.com")
-student3 = Student.new(firstname: "Семен", lastname: "Сидоров", surname: "Семенович")
+begin
+  # Создание объектов студентов
+  student1 = Student.new(firstname: "Иван", lastname: "Иванов", surname: "Иванович", phone: "+79991234567", email: "ivan@example.com")
+  student2 = Student.new(firstname: "Петр", lastname: "Петров", surname: "Петрович", telegram: "@petrov")
+  student3 = Student.new(firstname: "Семен", lastname: "Сидоров", surname: "Семенович")
 
-# Вывод информации о студентах
-puts student1
-puts "------------------"
-puts student2
-puts "------------------"
-puts student3
+  # Вывод информации о студентах
+  puts student1
+  puts "------------------"
+  puts student2
+  puts "------------------"
+  puts student3
+
+  # Пример создания без обязательных полей, выбросит ошибку
+  student_invalid = Student.new(lastname: "Петров", telegram: "@petrov")
+rescue ArgumentError => e
+  puts "Ошибка создания студента: #{e.message}"
+end
