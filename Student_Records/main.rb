0117a064d443897e3cb77d1654b009b1709b64ef
Maks0nchick <maksimkrapotkin@gmail.com>
2024-11-09 02:35:27 +0300

    |Lab №2|Zadanie №1.6|creat Hash|
    *** Продумать аргументы конструктора в форме ХЭШа.

--- Student_Records/main.rb
+++ Student_Records/main.rb
@@ -2,10 +2,10 @@
 
 require_relative 'student'
 
-# Создание объектов класса Student с разными комбинациями полей
-student1 = Student.new(id: 1, last_name: "Иванов", first_name: "Иван", middle_name: "Иванович", phone: "1234567890", telegram: "@ivanov", email: "ivanov@mail.com", github: "github.com/ivanov")
-student2 = Student.new(id: 2, last_name: "Петров", first_name: "Петр", middle_name: "Петрович", phone: "0987654321")
-student3 = Student.new(id: 3, last_name: "Сидоров", first_name: "Сидор", middle_name: "Сидорович", email: "sidorov@mail.com")
+# Создание объектов класса Student с передачей параметров через хэш
+student1 = Student.new({ id: 1, last_name: "Иванов", first_name: "Иван", middle_name: "Иванович", phone: "1234567890", telegram: "@ivanov", email: "ivanov@mail.com", github: "github.com/ivanov" })
+student2 = Student.new({ id: 2, last_name: "Петров", first_name: "Петр", middle_name: "Петрович", phone: "0987654321" })
+student3 = Student.new({ id: 3, last_name: "Сидоров", first_name: "Сидор", middle_name: "Сидорович", email: "sidorov@mail.com" })
 
 # Вывод информации о каждом студенте
 puts "Информация о студентах:"
--- Student_Records/student.rb
+++ Student_Records/student.rb
@@ -4,21 +4,21 @@ class Student
   # Используем attr_accessor для автоматической генерации геттеров и сеттеров
   attr_accessor :id, :last_name, :first_name, :middle_name, :phone, :telegram, :email, :github
 
-  # Конструктор с именованными аргументами, для необязательных полей задаём значение по умолчанию (nil)
-  def initialize(id:, last_name:, first_name:, middle_name:, phone: nil, telegram: nil, email: nil, github: nil)
-    @id = id
-    @last_name = last_name
-    @first_name = first_name
-    @middle_name = middle_name
-    @phone = phone
-    @telegram = telegram
-    @email = email
-    @github = github
+  # Конструктор принимает один аргумент - хэш параметров
+  def initialize(args = {})
+    @id = args[:id]
+    @last_name = args[:last_name]
+    @first_name = args[:first_name]
+    @middle_name = args[:middle_name]
+    @phone = args[:phone]
+    @telegram = args[:telegram]
+    @email = args[:email]
+    @github = args[:github]
   end
 
   # Метод to_s для вывода информации об объекте
   def to_s
-    info = "ID: #{@id}\nФИО: #{@last_name} #{@first_name} #{@middle_name}\n"
+    info = "ID: #{@id || 'не указан'}\nФИО: #{@last_name} #{@first_name} #{@middle_name}\n"
     info += "Телефон: #{@phone || 'не указан'}\n"
     info += "Телеграм: #{@telegram || 'не указан'}\n"
     info += "Почта: #{@email || 'не указана'}\n"
