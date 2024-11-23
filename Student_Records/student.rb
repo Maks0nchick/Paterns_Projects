46cfe34d6e695f7548d83cf53efcf3d4984c3ab7
Maks0nchick <maksimkrapotkin@gmail.com>
2024-11-23 13:01:11 +0300

    |Lab №2|Zadanie №1.7|Update number|
    Изменил проверку телефонного номера

--- Student_Records/student.rb
+++ Student_Records/student.rb
@@ -1,21 +1,44 @@
- # Используем attr_accessor для автоматической генерации геттеров и сеттеров
- attr_accessor :id, :last_name, :first_name, :middle_name, :phone, :telegram, :email, :github
+# student.rb
 
- # Конструктор принимает один аргумент - хэш параметров
- def initialize(args = {id:, last_name:, first_name:, middle_name:, phone: nil, telegram: nil, email: nil, github: nil})
-   @id = args[:id]
-   @last_name = args[:last_name]
-   @first_name = args[:first_name]
-   @middle_name = args[:middle_name]
-   @phone = args[:phone]
-   @telegram = args[:telegram]
-   @email = args[:email]
-   @github = args[:github]
+class Student
+  # Автоматическая генерация геттеров и сеттеров
+  attr_accessor :id, :last_name, :first_name, :middle_name, :telegram, :email, :github
+  attr_reader :phone # Геттер для телефона отдельно, чтобы добавить проверку в сеттер
+
+  # Метод класса для проверки валидности телефонного номера
+  def Student.valid_phone?(phone)
+    phone.match?(/\A\+?\d{10,15}\z/) # Телефон должен быть от 10 до 15 цифр, с опциональным "+"
   end
-  # Метод to_s для вывода информации об объекте
+
+  # Конструктор принимает хэш параметров
+  def initialize(args = {})
+    @id = args[:id]
+    @last_name = args[:last_name]
+    @first_name = args[:first_name]
+    @middle_name = args[:middle_name]
+    self.phone = args[:phone] # Используем сеттер для проверки телефона
+    @telegram = args[:telegram]
+    @github = args[:github]
+    @email = args[:email]
+  end
+
+  # Сеттер для телефона с проверкой
+  def phone=(value)
+    if value.nil? || Student.valid_phone?(value)
+      @phone = value
+    else
+      raise ArgumentError, "Недопустимый формат телефонного номера: #{value}"
+    end
+  end
+
+  # Метод для вывода информации об объекте
   def to_s
-    info = "ID: #{@id}\nФИО: #{@last_name} #{@first_name} #{@middle_name}\n"
-    info = "ID: #{@id || 'не указан'}\nФИО: #{@last_name} #{@first_name} #{@middle_name}\n"
+    info = "ID: #{@id || 'не указан'}\n"
+    info += "ФИО: #{@last_name} #{@first_name} #{@middle_name}\n"
     info += "Телефон: #{@phone || 'не указан'}\n"
     info += "Телеграм: #{@telegram || 'не указан'}\n"
     info += "Почта: #{@email || 'не указана'}\n"
+    info += "GitHub: #{@github || 'не указан'}"
+    info
+  end
+end
