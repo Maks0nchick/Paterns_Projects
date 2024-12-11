767bf3694aa40746dd392da291e98ffc97aefed1
Maks0nchick <maksimkrapotkin@gmail.com>
2024-12-11 16:28:54 +0300

    |Lab №2|Zadanie №4|update Student_short|

--- Student_Records/student_short.rb
+++ Student_Records/student_short.rb
@@ -1,23 +1,44 @@
-# Класс StudentShort наследует Person
-class StudentShort < Person
-  attr_reader :initials
+class StudentShort
+  # Поля только для чтения
+  attr_reader :id, :initials, :github, :contact
 
   # Конструктор 1: Принимает объект класса Student
-  def initialize(student)
-    super(id: student.id, github: student.github, contact: student.get_contact)
-    @initials = "#{student.last_name} #{student.first_name[0]}.#{student.middle_name.nil? ? '' : student.middle_name[0] + '.'}"
+  def self.creat_from_student(student)
+    new(
+    id: student.id
+    initials: "#{student.last_name} #{student.first_name[0]}.#{student.middle_name.nil? ? '' : student.middle_name[0] + '.'}"
+    github: student.github
+    contact: format_contact(student)
+    )
   end
+
+  private_class_method :new
 
   # Конструктор 2: Принимает ID и строку, содержащую информацию
-  def self.from_string(id, initials, github, contact)
-    new_obj = self.allocate
-    new_obj.send(:initialize, id: id, github: github, contact: contact)
-    new_obj.instance_variable_set(:@initials, initials)
-    new_obj
+  def initialize(id:, initials:, github:, contact:)
+    # Устанавливаем id
+    @id = id
+    @initials = initials
+    @github = github
+    @contact = contact
   end
 
-  # Переопределяем to_s для StudentShort
+  def self.creat_from_sring(string)
+    id, initials, github, contact = string.split(', ')
+    new(id: id, initials: initials, github: github, contact: contact)
+  end
+
+  # Метод для форматирования контактов
+  def format_contact(student)
+    contact_info = []
+    contact_info << "Телефон: #{student.phone}" if student.phone
+    contact_info << "Telegram: #{student.telegram}" if student.telegram
+    contact_info << "Email: #{student.email}" if student.email
+    contact_info.join(' ; ')
+  end
+
+  # Метод для вывода информации об объекте
   def to_s
-    "ID: #{@id}; ФИО: #{@initials}; GitHub: #{@github}; Контакты: #{@contact}"
+    "ID: #{@id}, ФИО: #{@initials}, GitHub: #{@github}, Контакты: #{@contact}"
   end
 end
