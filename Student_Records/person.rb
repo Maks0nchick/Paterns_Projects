548604f690ebf348c5b5b10cd9e9fc96fe4fadb2
Maks0nchick <maksimkrapotkin@gmail.com>
2024-12-11 16:56:58 +0300

    |Lab №2|Zadanie №5|update class Person|
    UPDATE

--- Student_Records/person.rb
+++ Student_Records/person.rb
@@ -1,78 +1,39 @@
-# Базовый класс, содержащий общую логику для студентов
-class Person
-  attr_accessor :id
-  attr_reader :last_name, :first_name, :middle_name, :email, :phone, :telegram, :github
+# Базовый класс для студентов
+class BaseStudent
+  attr_reader :id, :github
 
-  def self.valid_phone?(phone)
-    phone.match?(/\A\+?\d{10,15}\z/)
+  def initialize(id:, github: nil)
+    self.id = id
+    self.github = github unless github.nil?
   end
 
-  def self.valid_name?(name)
-    name.match?(/\A[\u0410-\u044Fa-zA-Z-]+\z/)
-  end
-
-  def self.valid_telegram?(telegram)
-    telegram.nil? || telegram.match?(/\A@[A-Za-z0-9_]+\z/)
-  end
-
-  def self.valid_email?(email)
-    email.nil? || email.match?(/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i)
+  # Валидаторы для id и GitHub
+  def self.valid_id?(id)
+    id.match?(/\A\d+\z/)
   end
 
   def self.valid_github?(github)
-    github.nil? || github.match?(/\Ahttps?:\/\/(www\.)?github\.com\/[A-Za-z0-9_-]+\/?\z/)
+    github.match?(%r{\Ahttps://github\.com/[-a-zA-Z0-9@:%_\+.~#=]+/[-a-zA-Z0-9._~%]+\z})
   end
 
-  def initialize(args = {})
-    @id = args[:id]
-    self.last_name = args[:last_name]
-    self.first_name = args[:first_name]
-    self.middle_name = args[:middle_name]
-    self.contacts = args
+  # Сеттер для id
+  def id=(id)
+    raise ArgumentError, "Неправильный id" unless self.class.valid_id?(id)
+
+    @id = id
   end
 
-  def last_name=(value)
-    raise ArgumentError, "Некорректная фамилия: #{value}" unless self.class.valid_name?(value)
-    @last_name = value
+  # Проверка наличия GitHub
+  def has_github?
+    !@github.nil? && !@github.empty?
   end
 
-  def first_name=(value)
-    raise ArgumentError, "Некорректное имя: #{value}" unless self.class.valid_name?(value)
-    @first_name = value
-  end
+  protected
 
-  def middle_name=(value)
-    raise ArgumentError, "Некорректное отчество: #{value}" unless value.nil? || self.class.valid_name?(value)
-    @middle_name = value
-  end
+  # Сеттер для GitHub
+  def github=(github)
+    raise ArgumentError, "Неправильный GitHub URL" unless self.class.valid_github?(github)
 
-  def contacts=(args = {})
-    @phone = args[:phone]
-    raise "Некорректный номер телефона" if @phone && !self.class.valid_phone?(@phone)
-
-    @telegram = args[:telegram]
-    raise "Некорректный телеграм аккаунт" if @telegram && !self.class.valid_telegram?(@telegram)
-
-    @email = args[:email]
-    raise "Некорректная почта" if @email && !self.class.valid_email?(@email)
-
-    @github = args[:github]
-    raise "Некорректный GitHub URL" if @github && !self.class.valid_github?(@github)
-  end
-
-  def get_contact
-    if @phone&.strip&.length&.positive?
-      "Телефон: #{@phone}"
-    elsif @telegram&.strip&.length&.positive?
-      "Телеграм: #{@telegram}"
-    elsif @email&.strip&.length&.positive?
-      "Почта: #{@email}"
-    else
-      "Контакты отсутствуют"
-    end
-  end
-
-  def to_s
-    "ID: #{@id || 'не указан'}\nФИО: #{@last_name} #{@first_name} #{@middle_name || ''}\nТелефон: #{@phone || 'не указан'}\nТелеграм: #{@telegram || 'не указан'}\nПочта: #{@email || 'не указана'}\nGitHub: #{@github || 'не указан'}"
+    @github = github
   end
 end
--- Student_Records/student_short.rb
+++ Student_Records/student_short.rb
@@ -1,24 +1,25 @@
-# Класс StudentShort, работающий с краткой информацией
-class StudentShort < Person
+# Класс StudentShort, наследующий от BaseStudent
+class StudentShort < BaseStudent
   attr_reader :initials, :contact
 
-  def self.create_from_student(student)
-    new(
-      id: student.id,
-      initials: student.get_short_name,
-      github: student.github,
-      contact: student.get_contact
-    )
-  end
-
-  def initialize(id:, initials:, github:, contact:)
-    @id = id
+  def initialize(id:, github: nil, initials:, contact:)
+    super(id: id, github: github)
     @initials = initials
-    @github = github
     @contact = contact
   end
 
-  def to_s
-    "ID: #{@id}, ФИО: #{@initials}, GitHub: #{@github}, Контакты: #{@contact}"
+  def self.create_from_student(student)
+    raise ArgumentError, "Некорректный объект Student" unless student.is_a?(Student)
+
+    new(
+      id: student.id,
+      github: student.github,
+      initials: student.get_short_name,
+      contact: student.send(:contact_info)
+    )
   end
-end 
+
+  def to_s
+    "ID: #{@id}, ФИО: #{@initials}, GitHub: #{@github || 'не указан'}, Контакты: #{@contact}"
+  end
+end
