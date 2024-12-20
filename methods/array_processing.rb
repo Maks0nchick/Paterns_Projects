0ebf2e63de29b26cd46116075d329a51f9b0cae9
Maks0nchick <maksimkrapotkin@gmail.com>
2024-12-21 02:21:49 +0300

    |Lab№3|Zadanie№1|Task№1.44|An array of numbers is given. It is necessary to check whether integers and real numbers alternate in it|
    Дан массив чисел. Необходимо проверить, чередуются ли в нем
    целые и вещественные числа.

--- methods/array_processing.rb
+++ methods/array_processing.rb
@@ -1,7 +1,16 @@
-# Метод для подсчёта локальных максимумов
-def count_local_maxima(arr)
-  # Проверяем соседей каждого элемента (кроме первого и последнего)
-  arr.each_cons(3).count do |left, center, right|
-    center > left && center > right
+# Метод для проверки, чередуются ли целые и вещественные числа в массиве
+def check_alternating_integers_and_floats(arr)
+  # Проходим по всему массиву, начиная с первого элемента
+  (0...arr.length - 1).each do |i|
+    current = arr[i]
+    next_num = arr[i + 1]
+
+    # Проверка типа текущего и следующего числа
+    if current.is_a?(Integer) && next_num.is_a?(Integer) || current.is_a?(Float) && next_num.is_a?(Float)
+      return false
+    end
   end
+
+  # Если пройден весь массив и не найдено ошибок чередования, возвращаем true
+  true
 end
--- methods/main.rb
+++ methods/main.rb
@@ -1,12 +1,12 @@
 require_relative 'array_processing'
 
 def read_array_from_file(filename)
-  # Чтение массива из файла
-  File.read(filename).split.map(&:to_i)
+  # Чтение массива из файла и преобразование в числа
+  File.read(filename).split.map { |x| x.include?('.') ? x.to_f : x.to_i }
 end
 
 def main
-  puts "Задача 1.32: Найти количество локальных максимумов"
+  puts "Задача 1.44: Проверка, чередуются ли целые и вещественные числа в массиве"
 
   # Проверяем существование файла
   filename = "array.txt"
@@ -19,9 +19,13 @@ def main
   array = read_array_from_file(filename)
   puts "Массив из файла: #{array}"
 
-  # Вызываем метод для подсчёта локальных максимумов
-  local_maxima_count = count_local_maxima(array)
-  puts "Количество локальных максимумов: #{local_maxima_count}"
+  # Вызываем метод проверки чередования целых и вещественных чисел
+  result = check_alternating_integers_and_floats(array)
+  if result
+    puts "Числа чередуются."
+  else
+    puts "Числа не чередуются."
+  end
 end
 
 main
