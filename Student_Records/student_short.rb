3fb14c314c85e254763bce6b6cdc659329657c88
Maks0nchick <maksimkrapotkin@gmail.com>
2024-12-21 11:32:40 +0300

    |Lab№2|Zadanie№3-4|Recovery class_Student_Short|
    Восстановил класс Student_Short

---
+++ Student_Records/student_short.rb
@@ -0,0 +1,32 @@
+require_relative 'person'
+
+class Student_short < Person
+
+  def self.about_student(student)
+    new(id: student.id, git: student.git, fullname: student.fullname, contact: student.contact)
+  end  
+
+  def self.from_sting(id:,string:)
+    fullname, contact, git= read_info_from_string(string)
+    new(id: id, fullname:fullname, contact:contact, git:git)
+  end
+  
+  def  self.read_info_from_string(string)
+    fullname,contact,git = string.split(', ')
+    return fullname,contact,git
+  end  
+  
+  def to_s
+    "\nID: #{@id} \nFullname: #{@fullname} \nGit: #{@git} \nContact: #{@contact}"
+  end
+
+  private attr_accessor :fullname, :contact 
+
+  private_class_method :new
+
+    def initialize(id: nil, fullname:nil , git: nil, contact: nil)
+      super(id: id, git: git)
+      @fullname = fullname
+      @contact = contact
+    end 
+end  
