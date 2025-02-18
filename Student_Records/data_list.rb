3d7f5638f14d7498bf7194fb8ba924afb85e2ed0
Maks0nchick <maksimkrapotkin@gmail.com>
2025-02-18 19:31:49 +0300

    |Lab№4|Zadanie№1.4|

--- Student_Records/data_list.rb
+++ Student_Records/data_list.rb
@@ -2,9 +2,21 @@ class Data_list
   def initialize(list)
     self.list = list
   end
-
+  
   def select(index)
     self.selected_elems = list[index]
+  end
+
+  def get_select(index)
+    self.selected_elems = list[index]
+    raise "Index out of range" if index < 0 || index >= list.length
+    self.selected_indexes << index
+  end
+
+  def get_names 
+  end
+
+  def get_data
   end
 
   private
